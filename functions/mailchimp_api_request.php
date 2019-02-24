458ca6e4dc33f7d3e7a8a2d109e26c6d72ba7d1a
YenTingWangTW <47776728+YenTingWangTW@users.noreply.github.com>
2019-02-24 22:02:07 +0800

    Update mailchimp_api_request.php

--- functions/mailchimp_api_request.php
+++ functions/mailchimp_api_request.php
@@ -21,19 +21,19 @@ function mailchimp_api_request($api_key, $type, $target, $timeout = 10, $data = 
 {
 	//setting url
 	$core_api_endpoint = 'https://<dc>.api.mailchimp.com/3.0/';
-    list(, $datacenter) = explode( '-', $api_key );
-    $api = str_replace( '<dc>', $datacenter, $core_api_endpoint);
+    	list(, $datacenter) = explode( '-', $api_key );
+    	$api = str_replace( '<dc>', $datacenter, $core_api_endpoint);
 	$url = $api . $target;
 	
 	//curl operations
 	$ch = curl_init($url);
 
 	curl_setopt($ch, CURLOPT_USERPWD, 'user:' . $api_key);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $type);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    	curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+    	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    	curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
+    	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $type);
+    	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
                                                                                                                    
 	// encode json to data if any
 	if ($data){
